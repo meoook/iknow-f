6583c0e8b71d117b7649c3eaef41bb1dbd7dada6
meok <mewooook@gmail.com>
2026-03-25 19:55:48 +0100

    tabbar

--- src/components/tapbar/index.tsx
+++ src/components/tapbar/index.tsx
@@ -1,5 +1,6 @@
 import style from './tapbar.module.scss'
 import IconSprite from '../../elements/icon/Icon'
+import { Link } from 'react-router-dom'
 
 interface TapBarProps {
   tags?: string[]
@@ -8,7 +9,18 @@ interface TapBarProps {
 export default function TapBar({ tags }: TapBarProps) {
   return (
     <div className={style.bar}>
-      <div>test</div>
+      <Link to='/' className={style.item}>
+        <IconSprite name='home' />
+        <span>Главная</span>
+      </Link>
+      <button className={style.item}>
+        <IconSprite name='search' />
+        <span>Поиск</span>
+      </button>
+      <Link to='/menu' className={style.item}>
+        <IconSprite name='menu' />
+        <span>Меню</span>
+      </Link>
     </div>
   )
 }
